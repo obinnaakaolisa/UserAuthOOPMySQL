ad0c98e856b94ababd0c9622877024ee68250949
Obinna Akaolisa <39627400+obinnaakaolisa@users.noreply.github.com>
2022-06-28 11:02:58 +0100

    Utilized OOP DB connection

--- classes/Dbh.php
+++ classes/Dbh.php
@@ -1,15 +1,18 @@
 <?php
 
 class Dbh {
-    public $hostname = "127.0.0.1";
-    public $username = "root";
-    public $password = "";
-    public $dbname = "zuriphp";
+    protected $hostname = "127.0.0.1";
+    protected $username = "root";
+    protected $password = "";
+    protected $dbname = "zuriphp";
 
     protected function connect() {
-        $conn = mysqli_connect($this->hostname, $this->username, $this->password, $this->dbname);
-        if($conn) return $conn;
+        $conn = new mysqli($this->hostname, $this->username, $this->password, $this->dbname);
+        if($conn->connect_error)
+        {
+            die ("<h1>Database Connection Failed</h1>");
+        }
+        //echo "Database Connected Successfully";
+        return $this->conn = $conn;
     }
-
-
 }
